73fe0c86b250cf87097c61d5c2a9b15773437eb8
Jan-Willem Gmelig Meyling <jan-willem@meylingmedia.nl>
2013-11-06 12:13:51 +0100

    Password decryption

--- src/packets/Auth_Login_Packet.java
+++ src/packets/Auth_Login_Packet.java
@@ -1,27 +1,16 @@
 package packets;
 
-import conquerServer.PasswordCrypter;
-
 /* Packet is always incoming, not outgoing. */  
 
 public class Auth_Login_Packet extends IncommingPacket {
-	private String accoutName; /* always 16 bytes. */ 
-	private String password;  /* always 16 bytes. */ 
-	private String serverName;  /* always 16 bytes. */ 
+	private String accoutName;
+	private String password;
+	private String serverName;
 	
 	public Auth_Login_Packet(short packetSize, PacketType packetType, byte[] packetData) {
 		super(packetSize, packetType, packetData);
 		accoutName	= this.getString(4,16);
-		
-		long[] password = new long[16];
-		for ( int i = 0; i < 16; i++ )
-			password[i] = (long) (data[i+20] & 0xff);
-		PasswordCrypter.decrypt(password);
-		String output = "";
-		for ( int i = 0; i < 16; i++)
-			output += (char) password[i];
-		
-		//password	= this.getString(20, 16);
+		password	= this.getPassword(20);
 		serverName	= this.getString(36, 16);
 	}
 	
--- src/packets/IncommingPacket.java
+++ src/packets/IncommingPacket.java
@@ -3,6 +3,8 @@ package packets;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
+import conquerServer.PasswordCrypter;
+
 public abstract class IncommingPacket extends Packet {
 	protected byte[] data;
 
@@ -19,27 +21,48 @@ public abstract class IncommingPacket extends Packet {
 		this.data = data;
 	}
 	
+	protected int getInt(int offset, int length) {
+		return ByteBuffer.wrap(Arrays.copyOfRange(data, offset, offset + length)).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
+	}
+
 	protected short getShort(int offset, int length) {
 		return ByteBuffer.wrap(Arrays.copyOfRange(data, offset, offset + length)).order(java.nio.ByteOrder.LITTLE_ENDIAN).getShort();
 	}
 	
-	protected long[] getLongArray(int offset, int length) {
-		long[] temp = new long[length];
-		for ( int i = 0; i < length; i++ )
-			temp[i] = (long) data[offset+i];
-//		System.arraycopy(data, 0, temp, offset, length);
-		return temp;
-	}
-	
-	protected int getInt(int offset, int length) {
-		return ByteBuffer.wrap(Arrays.copyOfRange(data, offset, offset + length)).order(java.nio.ByteOrder.LITTLE_ENDIAN).getInt();
-	}
-	
+	/**
+	 * Function to fetch a String from a package
+	 * @param offset
+	 * @param length
+	 * @return 
+	 */
 	protected String getString(int offset, int length) {
-		String output = "";
-		for ( int i = offset; i < (length + offset); i++)
-			if ( data[i] != 0 )
-				output += (char) data[i];
-		return output;
+		char[] output = new char[length];
+		for ( int i = 0; i < length; i++ )
+			if ( data[i+offset] != 0 )
+				output[i] = (char) data[i+offset];
+		return new String(output);
+	}
+
+	/**
+	 * Function to fetch and decrypt the password from the Auth package
+	 * @param {int} offset at which the password is at
+	 * @return {String} password
+	 */
+	protected String getPassword(int offset) {
+		// Convert split 16 bytes into 4 longs 
+		long[] encrypted = new long[4];
+		for ( int i = 0; i < 4; i++ )
+			for ( int j = 0; j < 4; j++ )
+				encrypted[i] += ((long) data[20+i*4+j] & 0xffL) << ( 8 * j);
+		// Decrypt password
+		PasswordCrypter.decrypt(encrypted);
+		// Convert decrypted long[4] array to chars
+		char[] decrypted = new char[16];
+		for ( int i = 0; i < 4; i++ )
+			for ( int j = 0; j < 4; j++ )
+				if ((encrypted[i] >> ( 4 - j - 1 << 3) & 0xFF) != 0) 
+					decrypted[i*4+3-j] = (char) (encrypted[i] >> ( 4 - j - 1 << 3) & 0xFF);
+		// Put char[] to string
+		return new String(decrypted);
 	}
 }
